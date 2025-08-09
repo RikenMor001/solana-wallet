7b8c2d85c4ad9688bf013a5b04a19d3b77d80b30
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-08-09 18:57:34 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import {Transaction, SystemProgram, Connection, PublicKey, LAMPORTS_PER_SOL} from "@solana/web3.js"
 import axios from "axios"
 
+const connection = Connection("https://solana-mainnet.g.alchemy.com/v2/i9WpXCP_SX2QikFfKnXFmZT--rjRGo65");
+
 function App() {
 
   function SendSol(){
     const ix = SystemProgram.transfer({
-      fromPubkey: new PublicKey("5wQRZdKExgNbNVtXRy2kEDGprDv3bAPp4qHvfChr553p"),
-      toPubkey: new PublicKey("6kCyq58u3w8Kami5dFpjyFWqD4stWbPiKCLGvY5g277B"),
+      fromPubkey: new PublicKey("5wQRZdKExgNbNVtXRy2kEDGprDv3bAPp4qHvfChrdZ3p"),
+      toPubkey: new PublicKey("6kCyq58u3w8Kami5dFpjyFWqD4stWbPiKCLGvY5g2XwB"),
       lamports: 0.001 * LAMPORTS_PER_SOL
     });
     const tx = new Transaction().add(ix);
@@ -22,7 +24,7 @@ function App() {
   return <div>
     <input type='text' placeholder='Enter your name'/>
     <input type='text' placeholder='Solana'/>
-    <button onClick = {SendSol}>
+    <button onClick={SendSol}>
       Send Solana
     </button>
   </div>
