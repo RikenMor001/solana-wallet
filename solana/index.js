b6ce852ddc111cc944c214e3abb18ed84142a0b9
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-08-09 17:15:31 -0400

    Update index.js

--- solana/index.js
+++ solana/index.js
@@ -6,12 +6,12 @@ const { Keypair } = require("@solana/web3.js");
 app.use(express.json())
 app.use(cors()) 
 
-app.post("api/v1/signup", (req, res) => {
+app.post("api/v1/signup", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     const KeyPair = new Keypair();
 
-    userModel.create({
+    await userModel.create({
         username: username,
         password: password,
         publicKey: KeyPair.publicKey.toString(),
@@ -23,7 +23,19 @@ app.post("api/v1/signup", (req, res) => {
 })
 
 app.post("api/v1/signin", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
     
+    const existingUser = userModel.findFirst({
+        username: username,
+        password: password
+    })
+
+    if (existingUser){
+        res.status(403).json({
+            error: "User already exists"
+        })
+    }
 })
 
 const port = 3000;
