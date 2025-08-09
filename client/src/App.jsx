2e7915b502384cfab6936fc3b1e9c4edc15410bd
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-08-09 17:52:37 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 import './App.css'
+import {Transaction, SystemProgram, Connection, PublicKey, LAMPORTS_PER_SOL} from "@solana/web3.js"
 
 function App() {
+
+  function SendSol(){
+    const ix = SystemProgram.transfer({
+      fromPubkey: PublicKey,
+      toPubkey: PublicKey,
+      lamports: 0.001 * LAMPORTS_PER_SOL
+    });
+    const transaction = new Transaction();
+  }
+
   return <div>
-    
+    <input type='text' placeholder='Enter your name'/>
+    <input type='text' placeholder='Solana'/>
+    <button>
+      Send Solana
+    </button>
   </div>
 }
 
